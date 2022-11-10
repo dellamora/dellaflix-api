6661a4aa57ef37ed0d793160de9f7d4bcc626928
dellamora <francielle@dellamora.dev>
2022-11-10 19:56:38 -0300

    added cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import express from "express"
 import * as dotenv from "dotenv"
 import movieRouter from "./modules/movie/movie.controller"
+import cors from "cors"
 dotenv.config()
 
 const app = express()
+app.use(cors())
 app.use("/movie", movieRouter)
 
 app.listen(4000, () => {console.log("RODANDO")})
